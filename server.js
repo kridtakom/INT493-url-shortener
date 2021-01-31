ab61b066f1e11766c5dc1bf3b6805ed2c996d6a9
Kridtakom <kridtakom.boom@mail.kmutt.ac.th>
2021-01-31 22:47:59 +0700

    [Add] route

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 const app = express();
+const indexRoute = require("./routes/index");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(cors());
+
+app.use("/", indexRoute);
+
+app.listen(port, () => {
+  console.log(`Start server at port. ${port}`);
 });
 
-app.listen(3000, () => {
-  console.log("Start server at port 3000.");
+app.use((req, res, next) => {
+  var err = new Error("Error Page Not Found");
+  err.status = 404;
+  next(err);
+  res.send("Page not found");
 });
