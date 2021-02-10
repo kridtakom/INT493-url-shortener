44ff8e913149a3c7c76397a6d7c8efc24a776b33
Mie Chayakorn <miechayakorn@gmail.com>
2021-02-10 23:34:51 +0700

    [Add] verifyUrl

--- routes/link.js
+++ routes/link.js
@@ -1,3 +1,4 @@
+const { text } = require('express');
 const express = require('express');
 const router = express.Router();
 const shortHash = require('short-hash');
@@ -9,38 +10,46 @@ const remoceSpace = (text) => {
     return text.replace(/\s/g, '')
 }
 
+const verifyUrl = (text) => {
+    return /^(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))$/.test(text)
+}
 
 router.post('/', function (req, res, next) {
     let { url } = req.body
     if (url) {
         url = remoceSpace(url)
-        let hashUrl = shortHash(url)
-        client.get(hashUrl, (err, reply) => {
-            if (!err) {
-                if (reply) {
-                    console.log("==== Form redis ===")
-                    let message = { link: base_url + hashUrl }
-                    res.status(200).json(message)
+        if (verifyUrl(url)) {
+            let hashUrl = shortHash(url)
+            client.get(hashUrl, (err, reply) => {
+                if (!err) {
+                    if (reply) {
+                        console.log("==== Form redis ===")
+                        let message = { link: base_url + hashUrl }
+                        res.status(200).json(message)
+                    } else {
+                        console.log("==== Save redis ====")
+                        client.set(hashUrl, url, (err) => {
+                            if (!err) {
+                                let message = { link: base_url + hashUrl }
+                                res.status(200).json(message)
+                            } else {
+                                console.error(err);
+                                res.status(500).send(err)
+                            }
+                        });
+                    }
                 } else {
-                    console.log("==== Save redis ====")
-                    client.set(hashUrl, url, (err) => {
-                        if (!err) {
-                            let message = { link: base_url + hashUrl }
-                            res.status(200).json(message)
-                        } else {
-                            console.error(err);
-                            res.status(500).send(err)
-                        }
-                    });
+                    console.log("error")
+                    console.error(err);
+                    res.status(500).send(err)
                 }
-            } else {
-                console.log("error")
-                console.error(err);
-                res.status(500).send(err)
-            }
-        });
+            });
+        } else {
+            res.status(400).send("Must be URL")
+        }
+
     } else {
-        res.status(500).send("Error")
+        res.status(400).send("Invalid Format")
     }
 });
 
