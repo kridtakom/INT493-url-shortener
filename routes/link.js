09f870ca97ceadc450e5edef1c8e61e28c89a227
Mie Chayakorn <miechayakorn@gmail.com>
2021-02-10 18:00:20 +0700

    [Add] short-hash

--- routes/link.js
+++ routes/link.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var shortHash = require('short-hash');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('TEST with a resource');
+const base_url = 'http://sh.a3.tnpl.me/l/'
+
+const remoceSpace = (text) => {
+    return text.replace(/\s/g, '')
+}
+
+router.post('/', function(req, res, next) {
+    let {url} = req.body
+    url = remoceSpace(url)
+    if(url){
+        let message = { link: base_url+shortHash(url)}
+        res.status(200).json(message)
+    }
 });
 
 module.exports = router;
