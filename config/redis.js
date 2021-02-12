0e1c25621d75a81af04f5da952bbb56c7473a1d9
Mie Chayakorn <miechayakorn@gmail.com>
2021-02-13 00:32:24 +0700

    [Add] fixbug setKeepAlive connecting

--- config/redis.js
+++ config/redis.js
@@ -1,12 +1,18 @@
 var redis = require("redis");
 
 var client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASS,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASS,
 });
+
+client.on("connect", function () {
+  var socket = client.stream;
+  socket.setKeepAlive(true, 30 * 1000);
+});
+
 client.on("error", function (error) {
-    console.error(error);
+  console.error(error);
 });
 
 module.exports = client;
