7f1e8e293abbd60ec97b999f13f51cdd47181fab
Mie Chayakorn <miechayakorn@gmail.com>
2021-02-12 22:57:28 +0700

    [Add] redis pass

--- config/redis.js
+++ config/redis.js
@@ -3,6 +3,7 @@ var redis = require("redis");
 var client = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT
+    password: process.env.REDIS_PASS
 });
 client.on("error", function (error) {
     console.error(error);
