004e7d54e108466fb554415f24a01693ce82d23f
Mie Chayakorn <miechayakorn@gmail.com>
2021-02-12 23:01:24 +0700

    [Edit] fix code error

--- config/redis.js
+++ config/redis.js
@@ -2,8 +2,8 @@ var redis = require("redis");
 
 var client = redis.createClient({
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
-    password: process.env.REDIS_PASS
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASS,
 });
 client.on("error", function (error) {
     console.error(error);
