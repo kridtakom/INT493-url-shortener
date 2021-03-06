49ec276321dca137613d9c83eaabcba1dacfd91d
Mie Chayakorn <miechayakorn@gmail.com>
2021-03-06 15:30:16 +0700

    [Update] lib hmset -> hset

--- URL-Shortener/routes/link.js
+++ URL-Shortener/routes/link.js
@@ -14,7 +14,7 @@ const verifyUrl = (text) => {
     return /^(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))$/.test(text)
 }
 
-router.post('/', function (req, res, next) {
+router.post('/', (req, res, next) => {
     let { url } = req.body
     if (url) {
         url = remoceSpace(url)
